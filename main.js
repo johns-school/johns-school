69d610bd5e6393f8012b52f24d61c6d33fb1f322
johns-school <johnsschool.dp@gmail.com>
2020-02-08 19:33:40 +0200

    commit

--- main.js
+++ main.js
@@ -104,21 +104,3 @@ let groupSlider = (function () {
     };
 }());
 let slider = groupSlider(".slider");
-
-window.scroll({
-    top: 2500, 
-    left: 0, 
-    behavior: 'smooth'
-  });
-  
-  // Scroll certain amounts from current position 
-  window.scrollBy({ 
-    top: 100, // could be negative value
-    left: 0, 
-    behavior: 'smooth' 
-  });
-  
-  // Scroll to a certain element
-  document.querySelector('href').scrollIntoView({ 
-    behavior: 'smooth' 
-  });
